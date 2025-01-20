391ce7e03752518d8d8be2ef410ad39b98e6cbb0
gabi droid <193822798+gabriel-droidel@users.noreply.github.com>
2025-01-20 14:56:18 +0100

    Make js basic calculation functions and connect styles with css

--- code.js
+++ code.js
@@ -5,3 +5,19 @@ function add(firstNumber, secondNumber){
 function subtract(firstNumber, secondNumber){
     return firstNumber-secondNumber;
 }
+
+function multiply(firstNumber, secondNumber){
+    return firstNumber*secondNumber;
+}
+
+function divide(firstNumber, secondNumber){
+    return firstNumber/secondNumber;
+}
+
+function operate(firstNumber, secondNumber, operation){
+
+}
+
+const keysBox = document.querySelector('.keys-box');
+const keys = keysBox.querySelectorAll('.key');
+console.log(keys);
